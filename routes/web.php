5da833d62ea852e5242c2ebb35ee0569f3d22a4a
Ilia <rudenok.it@gmail.com>
2025-12-10 16:24:32 +0300

    Fixes

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,24 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('task_statuses', TaskStatusController::class)->only(['index', 'show']);
-Route::resource('tasks', TaskController::class)->only(['index', 'show']);
-Route::resource('labels', LabelController::class)->only(['index', 'show']);
+Route::get('/task_statuses', [TaskStatusController::class, 'index'])->name('task_statuses.index');
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/labels', [LabelController::class, 'index'])->name('labels.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/task_statuses/create', [TaskStatusController::class, 'create'])->name('task_statuses.create');
     Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
     Route::get('/labels/create', [LabelController::class, 'create'])->name('labels.create');
+});
 
-    Route::resource('task_statuses', TaskStatusController::class)->except(['index', 'show', 'create']);
-    Route::resource('tasks', TaskController::class)->except(['index', 'show', 'create']);
-    Route::resource('labels', LabelController::class)->except(['index', 'show', 'create']);
+Route::get('/task_statuses/{task_status}', [TaskStatusController::class, 'show'])->name('task_statuses.show');
+Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+Route::get('/labels/{label}', [LabelController::class, 'show'])->name('labels.show');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('task_statuses', TaskStatusController::class)->only(['store', 'edit', 'update', 'destroy']);
+    Route::resource('tasks', TaskController::class)->only(['store', 'edit', 'update', 'destroy']);
+    Route::resource('labels', LabelController::class)->only(['store', 'edit', 'update', 'destroy']);
 });
 
 require __DIR__ . '/auth.php';
