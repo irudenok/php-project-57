0a232386113abfd1b7cfe5d4d1ce7c7b3ff0a703
Ilia <rudenok.it@gmail.com>
2025-12-10 12:57:19 +0300

    Fixes

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,13 @@ Route::resource('tasks', TaskController::class)->only(['index', 'show']);
 Route::resource('labels', LabelController::class)->only(['index', 'show']);
 
 Route::middleware('auth')->group(function () {
-    Route::resource('task_statuses', TaskStatusController::class)->except(['index', 'show']);
-    Route::resource('tasks', TaskController::class)->except(['index', 'show']);
-    Route::resource('labels', LabelController::class)->except(['index', 'show']);
+    Route::get('/task_statuses/create', [TaskStatusController::class, 'create'])->name('task_statuses.create');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::get('/labels/create', [LabelController::class, 'create'])->name('labels.create');
+
+    Route::resource('task_statuses', TaskStatusController::class)->except(['index', 'show', 'create']);
+    Route::resource('tasks', TaskController::class)->except(['index', 'show', 'create']);
+    Route::resource('labels', LabelController::class)->except(['index', 'show', 'create']);
 });
 
 require __DIR__ . '/auth.php';
